d6373c864b630ac43e23478289720d0bb3c28b6d
Cameron Planck <cameronplanck@gmail.com>
2023-04-12 20:36:40 -0400

    more updated

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -29,7 +29,9 @@ export default function Document() {
 
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+      </Head>
       <body id='root'>
         <Main />
         <NextScript />
