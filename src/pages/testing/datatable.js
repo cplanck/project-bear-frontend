b22b8d68fae031c76baafe06774158a44ceee3dd
Cameron Planck <cameronplanck@gmail.com>
2023-04-19 15:29:09 -0400

    getting closer updates...

--- src/pages/testing/datatable.js
+++ src/pages/testing/datatable.js
@@ -1,23 +1,3 @@
-import ExampleDataTable from "../../components/datatable/ExampleDataTable";
-import { useEffect, useState } from "react";
-
-export default function Datatable(){
-
-    const [data, setData] = useState('')
-
-    useEffect(()=>{const fetchData = async () => {
-        const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10eHFmanZrd3JhdHZzbmRyc3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA1NDk3MTMsImV4cCI6MTk5NjEyNTcxM30.Uub6YPIH99Mr-9D2X0_XkOHxyilTaE-yRHVdGUnHoy8'
-        const response = await fetch('https://mtxqfjvkwratvsndrszx.supabase.co/rest/v1/sign-up-emails', {headers: {'apikey': key}});
-
-        const json = await response.json();
-        console.log(json)
-        setData(json);
-      };
-  
-      fetchData();
-    }, []);
-
-    return(<div>{data[0]?.email}</div>)
 
 
-}
+
