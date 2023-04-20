d77d64412857c6ad38bc7246eb3d595cf895bca5
Cameron Planck <cameronplanck@gmail.com>
2023-04-20 10:25:11 -0400

    added CSV download

--- src/components/deployment/DeploymentData.js
+++ src/components/deployment/DeploymentData.js
@@ -8,9 +8,53 @@ import depstyles from './Deployment.module.css';
 import Modal from '@mui/material/Modal';
 import { Container } from '@mui/system';
 import { useState } from 'react';
+import { CSVLink, CSVDownload } from "react-csv";
 
 
 export default function DeploymentData(props){
+
+    const data = [
+        {
+          name: 'John',
+          lastName: 'Doe',
+          address: '261 Erdman Ford',
+          city: 'East Daphne',
+          state: 'Kentucky',
+          id: 10
+        },
+        {
+            name: 'Jane',
+            lastName: 'Doe',
+            address: '769 Dominic Grove',
+            city: 'Columbus',
+            state: 'Ohio',
+            id: 11
+        },
+        {
+          name: 'Joe',
+          lastName: 'Doe',
+          address: '566 Brakus Inlet',
+          city: 'South Linda',
+          state: 'West Virginia',
+          id: 12
+        },
+        {
+          name: 'Kevin',
+          lastName: 'Vandy',
+          address: '722 Emie Stream',
+          city: 'Lincoln',
+          state: 'Nebraska',
+          id: 13
+        },
+        {
+          name:'Joshua',
+          lastName: 'Rolluffs',
+          address: '32188 Larkin Turnpike',
+          city: 'Charleston',
+          state: 'South Carolina',
+          id: 14
+        },
+      ];
 
     const [dataModalOpen, setDataModalOpen] = useState(false)
     return(
@@ -21,7 +65,7 @@ export default function DeploymentData(props){
             </div>
             <div className={depstyles.deploymentDetailsTableRow}>
                 <span className='boldText'>CSV</span>
-                <button className={['darkThemeBlueText', 'textButton'].join(" ")}><DownloadIcon className={['darkThemeBlueText'].join(' ')} style={{paddingRight: '5px'}} fontSize='small'/>Download</button>
+                <CSVLink filename={props.deployment.name + '.csv'} className={['darkThemeBlueText', 'textButton'].join(" ")} data={data}><DownloadIcon className={['darkThemeBlueText'].join(' ')} style={{paddingRight: '5px'}} fontSize='small'/>Download</CSVLink>
             </div>
             <DataTable open={dataModalOpen} setDataModalOpen={setDataModalOpen}/>
         </div>
